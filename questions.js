94a4096150a2f1d79ef0b497261fa9e6aeea5cf2
Nicholas Cox <nick.a.cox@gmail.com>
2019-11-13 10:11:40 -0600

    moving JSON into new file

---
+++ questions.js
@@ -0,0 +1,57 @@
+var questions = [
+    {
+        "question": "What tag do you use to reference an external CSS stylesheet?",
+        "answers": ['<link>', '<style>', '<css>', '<p>'],
+        "correct": "<link>"
+    },
+    {
+        "question": "What tag do you use to reference an external JavaScript file?",
+        "answers": ['<style>', '<css>', '<script>', '<js>'],
+        "correct": "<script>"
+    },
+    {
+        "question": "Which of these is NOT a way to reference colors?",
+        "answers": ['rbga value', 'hex code', 'by name', 'by emotion evoked'],
+        "correct": "by emotion evoked"
+    },
+    {
+        "question": "How do you add a value to the end of a JavaScript array?",
+        "answers": ['.add()', '.push()', '.pop()', '.end()'],
+        "correct": ".push()"
+    },
+    {
+        "question": "How many columns are in the Bootstrap grid?",
+        "answers": ['6', '3', '12', '10'],
+        "correct": "12"
+    },
+    {
+        "question": "Which command uploads your changes to GitHub?",
+        "answers": ['git pull', 'git status', 'git push', 'git commit'],
+        "correct": "git push"
+    },
+    {
+        "question": "How do you reference the JQuery library in JavaScript?",
+        "answers": ['#', '&', '@', '$'],
+        "correct": "$"
+    },
+    {
+        "question": "How do you update text in vanilla JavaScript?",
+        "answers": ['item.textContent', 'item.content', 'item.words', 'item.innerText'],
+        "correct": "item.textContent"
+    },
+    {
+        "question": '"Local" JavaScript variables are accessible where?',
+        "answers": ['Anywhere', 'In the function where they are declared', 'In their parent functions', 'Only in "for" loops'],
+        "correct": "In the function where they are declared"
+    },
+    {
+        "question": 'What does "HTML" stand for?',
+        "answers": ['Holy Turkey Monster Lizard', 'HiT Me, Linda', 'HyperText Markup Language', 'How The Marbles Land'],
+        "correct": "HyperText Markup Language"
+    },
+    {
+        "question": 'What does DOM stand for?',
+        "answers": ['Do Ow to Me', 'Document Object Model', "Dunkin' Or Maybe (somewhere else)", 'Donkly Onkly Monkly'],
+        "correct": "Document Object Model"
+    }
+];
